ea995f4d0f96bc9b84db65677f0b317263ba7fdc
Nayan <nayanmordiya172@gmail.com>
2021-12-11 13:40:15 +0530

    Day 5 completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,56 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/utils/routes.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({ Key? key }) : super(key: key);
+  const LoginPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return  Material(
-      color: Colors.white,
-      child: Column(
-    children: [Image.asset("assets/images/login_image.png",
-      fit: BoxFit.cover,
-        ),
-        const Text("Welcome",
-        style: TextStyle(
-          fontSize: 25,
-          fontWeight: FontWeight.bold, 
-        ),
-        ),
-        const SizedBox(
-          height: 20.0,
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+    return Material(
+        color: Colors.white,
+        child: SingleChildScrollView(
           child: Column(
             children: [
-              TextFormField(
-                decoration: const InputDecoration(
-                  hintText: "Enter username",
-                  labelText: "Username",
-                ),
+              Image.asset(
+                "assets/images/login_image.png",
+                fit: BoxFit.cover,
               ),
-              TextFormField(
-                obscureText: true,
-                decoration: const InputDecoration(
-                  hintText: "Enter password",
-                  labelText: "Password",
+              const Text(
+                "Welcome",
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
               const SizedBox(
                 height: 20.0,
               ),
-              ElevatedButton(
-                child: const Text("LOGIN"),
-                style: TextButton.styleFrom(),
-                onPressed: () {
-                  print("Hii Chats");
-                }, 
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                    vertical: 16.0, horizontal: 32.0),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        hintText: "Enter username",
+                        labelText: "Username",
+                      ),
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: const InputDecoration(
+                        hintText: "Enter password",
+                        labelText: "Password",
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 40.0,
+                    ),
+                    ElevatedButton(
+                      child: const Text("LOGIN"),
+                      style: TextButton.styleFrom(minimumSize: Size(150, 40)),
+                      onPressed: () {
+                        Navigator.pushNamed(context, MyRouts.homeRoute);
+                      },
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
-        ),
-      ],
-    ));
+        ));
   }
 }
