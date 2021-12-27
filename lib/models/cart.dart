fbf3767ccb1f2381369f1f5d0e24dd4b41d0ff6b
Nayan <nayanmordiya172@gmail.com>
2021-12-27 11:04:28 +0530

    Day 23 completed

---
+++ lib/models/cart.dart
@@ -0,0 +1,35 @@
+import 'package:flutter_application_1/models/catalog.dart';
+
+class CartModel {
+  late CatalogModel _catalog;
+
+  //Collecton of IDs - store Ids of each item
+  final List<int> _itemIds = [];
+
+  //Get Catalog
+  CatalogModel get catalog => _catalog;
+
+  set catalog(CatalogModel newCatalog) {
+    assert(newCatalog != null);
+    _catalog = newCatalog;
+  }
+
+  //Get items in the cart
+  List<Item> get items => _itemIds.map((id) => _catalog.getById(id)).toList();
+
+  //Get total price
+  num get totalPrice =>
+      items.fold(0, (total, current) => total + current.price);
+
+  //Add Item
+
+  void add(Item item) {
+    _itemIds.add(item.id);
+  }
+
+  //Remove Item
+
+  void remove(Item item) {
+    _itemIds.remove(item.id);
+  }
+}
--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:velocity_x/velocity_x.dart';
 
 class CartPage extends StatelessWidget {
-  const CartPage({ Key? key }) : super(key: key);
+  const CartPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -21,13 +21,12 @@ class CartPage extends StatelessWidget {
           CartTotal(),
         ],
       ),
-      
     );
   }
 }
 
 class CartTotal extends StatelessWidget {
-  const CartTotal({ Key? key }) : super(key: key);
+  const CartTotal({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -38,21 +37,25 @@ class CartTotal extends StatelessWidget {
         children: [
           "\$9999".text.xl5.color(context.theme.accentColor).make(),
           30.widthBox,
-          ElevatedButton(onPressed: () {}, 
+          ElevatedButton(
+            onPressed: () {
+              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                content: "Buying not supported yet".text.make(),
+              ));
+            },
             style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all(context.theme.buttonColor)),
-          child: "Buy".text.white.make(),
+                backgroundColor:
+                    MaterialStateProperty.all(context.theme.buttonColor)),
+            child: "Buy".text.white.make(),
           ).w32(context)
         ],
       ),
-      
     );
   }
 }
 
-
 class CartList extends StatefulWidget {
-  const CartList({ Key? key }) : super(key: key);
+  const CartList({Key? key}) : super(key: key);
 
   @override
   State<CartList> createState() => _CartListState();
@@ -66,7 +69,7 @@ class _CartListState extends State<CartList> {
       itemBuilder: (context, index) => ListTile(
         leading: Icon(Icons.done),
         trailing: IconButton(
-          icon: Icon(Icons.remove_circle_outline), 
+          icon: Icon(Icons.remove_circle_outline),
           onPressed: () {},
         ),
         title: "Item 1".text.make(),
